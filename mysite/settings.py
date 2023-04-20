5e11c4679aa536aeb569c5f6cab896377b97a690
Lina Datkunaite <l.datkunaite@gmail.com>
2023-04-20 21:30:25 +0300

    added Author, Book, Genre, Bookinstance models and added models to admin site

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # MY APSS
+
+    # MY APSS - butina prideti
     'library',
     # THIRD PARTY APPS
 ]
