4c3f001706351eadb989cb09204ccbbabf3df2a6
Lina Datkunaite <l.datkunaite@gmail.com>
2023-04-25 21:06:46 +0300

    making admin settings

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from .models import *
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'display_books')
 
-from .models import Author, Book, Genre, Bookinstance
 
-admin.site.register(Author)
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'description', 'display_genre')
+    search_fields = ('title', )
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    # def available_on(self, obj):
+    #     return obj.due_back.strftime('%Y-%m-%d')
+
+    list_display = ('book', 'book_status', 'due_back')
+
+    list_filter = ('book_status', 'due_back')
+    fieldsets = (
+        ('General', {'fields': ('instance_id', 'book')}),
+        ('Availability', {'fields': ('book_status', 'due_back')}),
+    )
+    search_fields = ('instance_id', 'book__title')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
-admin.site.register(Bookinstance)
+admin.site.register(Bookinstance, BookInstanceAdmin)
